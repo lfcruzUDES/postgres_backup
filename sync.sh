656c4ac158e58a512fd75082dd4d493aa9c02dfc
Luis Fernando Cruz Carrillo <lfcruz@udes.edu.mx>
2020-02-05 11:11:50 -0600

    sync OK

---
+++ sync.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Sync db.bak files with remote host in the same network. 
+# Requires rsync
+# Author: Luis Fernando Cruz Carrillo
+# Email: lfcruz@udes.edu.mx - quattrococodrilo@gmail.com
+
+backupDir=""
+remoteDir=""
+user=""
+host=""
+
+
+lastback=$(find $backupDir -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
+
+rsync -avz $lastback $user@$host:$remoteDir
